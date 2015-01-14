e6d4bfe0eb1c797ee808dc306b9e816f64505b48
Sebastian <wahani@gmail.com>
2015-01-14 08:27:43 +0100

    update workflow

--- build.R
+++ build.R
@@ -1,4 +1,4 @@
-knitPost <- function(input, base.url = "/", outPath = "source/_posts/") {
+knitPost <- function(input, base.url = "/", outPath = "source/_posts/", output = NULL) {
   #input <- "2014-02-21-PlotNo1.Rmd"
   opts_knit$set(base.url = base.url)
   fig.path <- paste0("figs/", sub(".Rmd$", "", basename(input)), "/")
@@ -10,13 +10,18 @@ knitPost <- function(input, base.url = "/", outPath = "source/_posts/") {
   opts_chunk$set(fig.height=5)
   render_jekyll(highlight = "pygments", extra="linenos")
   knit(input,
-       output = paste0(outPath, sub(".Rmd$", ".md", basename(input)), ""),
+       output = if (is.null(output)) paste0(outPath, sub("\\.[R|r]md$", ".md", basename(input)), "") else output,
        envir = parent.frame())
 }
 # 
+
 library(knitr)
-files <- list.files("_rmd", pattern = "[R|r]md", full.names = TRUE)
-lapply(files, knitPost)
+# files <- list.files("_rmd", pattern = "[R|r]md", full.names = TRUE)
+# lapply(files, knitPost)
+
+args <- commandArgs(TRUE)
+knitPost(args[1], output = args[2])
+# knitPost("_rmd/2015-01-13-Setup-Jekyll-Octopress-and-knitr-for-workflow.rmd")
 
 # Getting things straight with folders...
 system("cp -f -r figs source")
